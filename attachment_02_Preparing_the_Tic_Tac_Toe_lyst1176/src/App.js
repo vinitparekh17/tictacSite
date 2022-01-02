54f3d90109c1ba5427b2cf979d5c5956872a0939
Vinit Parekh <vinitrevaliya@gmail.com>
2022-01-02 17:45:02 +0530

    done

---
+++ attachment_02_Preparing_the_Tic_Tac_Toe_lyst1176/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Icon from "./components/Icon";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Icon />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ attachment_02_Preparing_the_Tic_Tac_Toe_lyst1176/src/components/Icon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaTimes, FaPen, FaRegCircle } from "react-icons/fa";
+
+const Icon = ({ name }) => {
+  return (
+    <h1>
+      <FaTimes className="icon" />
+    </h1>
+  );
+};
+
+export default Icon;
